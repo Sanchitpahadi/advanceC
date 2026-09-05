d302bcf3ede380c88a29aca5dd097f7ad6feeb2b
Sanchitpahadi <sanchit.pahadi4u@gmail.com>
2026-09-05 08:38:34 +0000

    simple linkedlist example

--- recursion.c
+++ recursion.c
@@ -3,7 +3,7 @@
 
     typedef struct Vector
     { 
-        int capacity;
+        int capacity ;
         struct core * c;
         struct Vector * next;
     }Vector;
@@ -42,15 +42,48 @@ void r_f()
 
 int main()
 {
-    struct core v = {4,5};
+    struct core c1 = {4,8};
+    struct core c2 = {5,9};
+    struct core c3 = {6,10};
+    struct core c4 = {7,11};
+
     Vector* v1 = (Vector *) malloc(sizeof(Vector));
-    v1->c = &v;
-    v1->next = NULL;
+    Vector* v2 = (Vector *) malloc(sizeof(Vector));
+    Vector* v3 = (Vector *) malloc(sizeof(Vector));
+    Vector* v4 = (Vector *) malloc(sizeof(Vector));
 
-    printf("%d\n", v1->c->x);
+    v1->c = &c1;
+    v2->c = &c2;
+    v3->c = &c3;
+    v4->c = &c4;
+
+    v1->next = v2;
+    v2->next = v3;
+    v3->next = v4;
+    v4->next = NULL;
+
+
+    printf("%d ,", v1->c->x);
     printf("%d\n", v1->c->y); 
- 
+    
+    if(v1->next != NULL)
+    {
+        printf("%d ,", v1->next->c->x);
+        printf("%d\n", v1->next->c->y);
 
+    }
+    if(v2->next != NULL)
+    {
+        printf("%d ,", v2->next->c->x);
+        printf("%d\n", v2->next->c->y);
+
+    }
+    if(v3->next != NULL)
+    {
+        printf("%d ,", v3->next->c->x);
+        printf("%d\n", v3->next->c->y);
+
+    }
     free(v1);
     return 0;
 }
